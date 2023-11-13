23bc7b843441c40ce1df8cc29784bf544f9d66c5
Vaishnavi Sontakke <129650128+Vaishnavi0903@users.noreply.github.com>
2023-11-13 10:35:28 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,12 @@
+const data = 
+[
+    {name:'Vaishnavi Sontakke' , email:'vaish@text.com'},
+    {name:'Vaish Sontakke' , email:'vaish@text.com'},
+    {name:'Sakshi Chavan' , email:'vaish@text.com'},
+    {name:'Priyanka Ingale' , email:'vaish@text.com'},
+    {name:'Pratiksha Bajode' , email:'vaish@text.com'},
+    {name:'Prem Satpue' , email:'vaish@text.com'}
+]
+
+
+module.exports = data;
